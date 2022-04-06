7f79c6771f0e29bec4632d6251b14c360f6122e3
kamal <kamaldro97@gmail.com>
2022-04-06 19:58:15 +0530

    deployed

--- src/index.js
+++ src/index.js
@@ -7,20 +7,8 @@ import { PusherProvider } from "@harelpls/use-pusher";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
 const config = {
-  // required config props
-  clientKey: process.env.REACT_APP_CLIENT_KEY, // Added client in a env file to secure this from outsiders
+  clientKey: process.env.REACT_APP_CLIENT_KEY,
   cluster: "ap2",
-
-  // optional if you'd like to trigger events. BYO endpoint.
-  // see "Trigger Server" below for more info
-  triggerEndpoint: "/pusher/trigger",
-
-  // required for private/presence channels
-  // also sends auth headers to trigger endpoint
-  authEndpoint: "/pusher/auth",
-  auth: {
-    headers: { Authorization: "Bearer token" },
-  },
 };
 ReactDOM.render(
   <React.StrictMode>
